80c2a974419013ec48779a630bf49616845ee2b4
Alejandro <alevelara@gmail.com>
2023-02-01 21:06:17 +0100

    Adding feature and mapper with mapster

---
+++ TakeControl/takecontrol.Domain/Dtos/Clubs/ClubDto.cs
@@ -0,0 +1,18 @@
+﻿using takecontrol.Domain.Models.Addresses;
+
+namespace takecontrol.Domain.Dtos.Clubs;
+
+public sealed class ClubDto
+{
+    public Guid Id { get; set; }
+
+    public Guid AddresId { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public string Name { get; set; }
+
+    public string Code { get; set; }
+
+    public Address Address { get; set; }
+}
