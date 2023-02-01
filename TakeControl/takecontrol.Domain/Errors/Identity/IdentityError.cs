d9ed09292e4d23ee70ace0e5bbc6119abfebd050
juancr <canete86@gmail.com>
2023-02-01 22:42:13 +0100

    Generated token and check for errors.

--- TakeControl/takecontrol.Domain/Errors/Identity/IdentityError.cs
+++ TakeControl/takecontrol.Domain/Errors/Identity/IdentityError.cs
@@ -16,5 +16,6 @@ public sealed class IdentityError : DomainError
     public static IdentityError UserAlreadyExistsWithThisEmail = new IdentityError(1506, "This email is already used.");
     public static IdentityError UserAlreadyExistsWithThisUserName = new IdentityError(1507, "This username is already used.");
     public static IdentityError ErrorDuringUserRegistration = new IdentityError(1508, "Something happened during user registration.");
-    public static IdentityError ErrorChangingPassword = new IdentityError(1508, "Something happened during password update.");
+    public static IdentityError ErrorChangingPassword = new IdentityError(1509, "Something happened during password update.");
+    public static IdentityError ErrorGeneratingUpdatePassword = new IdentityError(1509, "Something happened during password update.");
 }
