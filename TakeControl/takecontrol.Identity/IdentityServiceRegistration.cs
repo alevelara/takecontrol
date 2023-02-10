d4557d2dd95ebb24198cf9b2883fa7e085b83413
Alejandro <alevelara@gmail.com>
2023-02-10 17:41:18 +0100

    Adding authetincation service registration

--- TakeControl/takecontrol.Identity/IdentityServiceRegistration.cs
+++ TakeControl/takecontrol.Identity/IdentityServiceRegistration.cs
@@ -1,10 +1,10 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
-using System.Text;
 using takecontrol.Application.Contracts.Identity;
 using takecontrol.Domain.Models.ApplicationUser.Options;
 using takecontrol.Identity.Models;
