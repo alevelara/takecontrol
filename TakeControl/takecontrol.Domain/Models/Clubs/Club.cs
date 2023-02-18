25f80d07dff75b0f663829f9b6eab9df44b226f2
Alejandro <alevelara@gmail.com>
2023-02-18 14:56:54 +0100

    Adding PlayerClub entity

--- TakeControl/takecontrol.Domain/Models/Clubs/Club.cs
+++ TakeControl/takecontrol.Domain/Models/Clubs/Club.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using takecontrol.Domain.Models.Addresses;
 using takecontrol.Domain.Models.Clubs.ValueObjects;
+using takecontrol.Domain.Models.PlayerClubs;
 using takecontrol.Domain.Primitives;
 
 namespace takecontrol.Domain.Models.Clubs;
@@ -23,6 +24,7 @@ public class Club : BaseDomainModel
     public string Code { get; private set; }
 
     public virtual Address Address { get; set; }
+    public virtual ICollection<PlayerClub> PlayerClubs { get; set; }
 
     private Club(Guid id, Guid addresId, Guid userId, string name, string code)
     {
