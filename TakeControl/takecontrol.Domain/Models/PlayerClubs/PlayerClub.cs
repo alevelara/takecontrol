0b63391041bb4fac8841f8f5d13c6614aea50e2c
Alejandro <alevelara@gmail.com>
2023-02-18 20:35:42 +0100

    Adding unit test

--- TakeControl/takecontrol.Domain/Models/PlayerClubs/PlayerClub.cs
+++ TakeControl/takecontrol.Domain/Models/PlayerClubs/PlayerClub.cs
@@ -1,4 +1,6 @@
-﻿using takecontrol.Domain.Models.Clubs;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using takecontrol.Domain.Models.Clubs;
 using takecontrol.Domain.Models.PlayerClubs.ValueObjects;
 using takecontrol.Domain.Models.Players;
 using takecontrol.Domain.Primitives;
@@ -8,7 +10,13 @@ namespace takecontrol.Domain.Models.PlayerClubs;
 public class PlayerClub : BaseDomainModel
 {
     public Guid Id { get; private set; }
+
+    [Required]
+    [ForeignKey("FK_PlayerId")]
     public Guid PlayerId { get; private set; }
+
+    [Required]
+    [ForeignKey("FK_ClubId")]
     public Guid ClubId { get; private set; }
 
     public virtual Player Player { get; private set; }
