cb7ed235987b1c9fe103fdf1fe07429b1b8cf648
Alejandro <alevelara@gmail.com>
2023-03-04 23:22:05 +0100

    Applying dotnet format

--- TakeControl/takecontrol.Application/Features/Players/Commands/JoinToClub/JoinToClubCommandValidator.cs
+++ TakeControl/takecontrol.Application/Features/Players/Commands/JoinToClub/JoinToClubCommandValidator.cs
@@ -4,17 +4,17 @@ namespace takecontrol.Application.Features.Players.Commands.JoinToClub;
 
 public class JoinToClubCommandValidator : AbstractValidator<JoinToClubCommand>
 {
-	public JoinToClubCommandValidator()
-	{
-		RuleFor(c => c.Code)
-			.NotEmpty()
-			.WithMessage("Code can not be empty");
+    public JoinToClubCommandValidator()
+    {
+        RuleFor(c => c.Code)
+            .NotEmpty()
+            .WithMessage("Code can not be empty");
 
-		RuleFor(c => c.ClubId)
-			.NotNull()
-			.NotEmpty()
-			.NotEqual(Guid.Empty)
-			.WithMessage("ClubId can not be empty");
+        RuleFor(c => c.ClubId)
+            .NotNull()
+            .NotEmpty()
+            .NotEqual(Guid.Empty)
+            .WithMessage("ClubId can not be empty");
 
         RuleFor(c => c.PlayerId)
             .NotNull()
