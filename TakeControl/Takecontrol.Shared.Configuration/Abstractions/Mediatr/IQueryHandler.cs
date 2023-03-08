fe51cec91236f3e5fd7541e0cd5781f52945433c
Alejandro <alevelara@gmail.com>
2023-03-08 21:26:26 +0100

    Refactoring #1: Creating modules

---
+++ TakeControl/Takecontrol.Shared.Configuration/Abstractions/Mediatr/IQueryHandler.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Takecontrol.Shared.Application.Abstractions.Mediatr;
+
+public interface IQueryHandler<in TQuery, TResponse> : IRequestHandler<TQuery, TResponse>
+    where TQuery : IQuery<TResponse>
+{
+}
---
+++ TakeControl/Takecontrol.Shared.Domain/Primitives/DomainError.cs
@@ -0,0 +1,14 @@
+﻿namespace takecontrol.Domain.Primitives;
+
+public class DomainError
+{
+    public string Message { get; set; }
+    public int CodeId { get; set; }
+
+    public DomainError(int codeId, string message)
+    {
+        Message = message;
+        CodeId = codeId;
+    }
+
+}
