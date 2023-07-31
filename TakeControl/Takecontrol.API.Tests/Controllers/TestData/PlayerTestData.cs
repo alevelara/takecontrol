05f54a0da3fd7acbfe45dfc652d6daf53f05b6e7
Alejandro <alevelara@gmail.com>
2023-07-31 20:08:59 +0200

    Testing API for JoinToMatch endpoint

---
+++ TakeControl/Takecontrol.API.Tests/Controllers/TestData/PlayerTestData.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http.Json;
+using Takecontrol.User.Domain.Messages.Players.Requests;
+
+namespace Takecontrol.API.Tests.Controllers.TestData;
+
+public static class PlayerTestData
+{
+    private const string RegisterPlayerEndpoint = "api/v1/player/Register";
+
+    public static async Task RegisterPlayerForTest(HttpClient httpClient)
+    {
+        var request = new RegisterPlayerRequest(
+            Email: "email2@test.com",
+            Name: "nameTest",
+            Password: "Password123!",
+            AvgNumberOfMatchesInAWeek: 1,
+            NumberOfClassesInAWeek: 1,
+            NumberOfYearsPlayed: 1);
+
+        await httpClient.PostAsJsonAsync(RegisterPlayerEndpoint, request, default);
+    }
+}
---
+++ TakeControl/Takecontrol.Shared.Tests/Repositories/Reservations/TestReservationWriteRepository.cs
@@ -0,0 +1,20 @@
+﻿using Takecontrol.Matches.Domain.Models.Reservations;
+using Takecontrol.Shared.Tests.MockContexts;
+
+namespace Takecontrol.Shared.Tests.Repositories.Reservations;
+
+public class TestReservationWriteRepository
+{
+    private readonly TakeControlMatchesDb _dbContext;
+
+    public TestReservationWriteRepository(TakeControlMatchesDb dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task AddReservationhAsync(Reservation reservation)
+    {
+        await _dbContext.Context.Set<Reservation>().AddAsync(reservation);
+        await _dbContext.Context.SaveChangesAsync();
+    }
+}
