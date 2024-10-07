a236ece8bf5ba366ecf07a8bbc53769efc269e2c
Alejandro <alevelara@gmail.com>
2024-10-07 21:03:49 +0200

    Creating the console project

---
+++ TakeControl/Takecontrol.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Spectre.Console;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+var host = builder.Build();
+
+builder.Services.AddHttpClient("api", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7167");
+    client.DefaultRequestHeaders.Add("Accept", "application/json");
+});
+
+AnsiConsole.MarkupLineInterpolated($"[underline red]Hello[/] World!");
+
+await host.RunAsync();
